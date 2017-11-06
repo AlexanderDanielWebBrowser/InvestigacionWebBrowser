e4dec27cc2083f2b58285b0dcdda35446cfd4fb9
Daniel <djdany.djesus@gmail.com>
2017-11-06 10:29:02 +0100

    v2.0 GUI implementation
    Se ha añadido una GUI

--- TrabajoWebBrowser.py
+++ TrabajoWebBrowser.py
@@ -1,47 +1,47 @@
-# Importa el webbrowser para poder usar el navegador
-# Importa el os para poder usar comandos de windows como el cls
 import webbrowser, os
+from tkinter import *
+from tkinter import messagebox
 
-# INICIO Alexander Daniel WebBrowser
-salida='1'
-while(salida!='0'):
-    opcion='0'
-    # Lanza el comando "cls" a través del sistema
-    os.system('cls')
-    print("Introduce la URL a navegar o introduce 0 para navegar por defecto: ")
-    url = input()
-    if(url == '0'):
-        url = "https://esviernes.com/"
+def navegar():
+    opcion = v.get()
 
-    # Registra el navegador que queremos usar, al no usarlo coge el por defecto
-    # Da un error close_fds que no es compatible con windows.
-    ## webbrowser.register(name, constructor, instance=None)
+    if(e.get() == ''):
+        e.insert(0, "www.esviernes.com")
 
-    # Asignamos el navegador por defecto con get() y si no lo encuentra lanza una excepcion
+    url = e.get()
+
     try:
         navegador = webbrowser.get('windows-default')
-    # Excepción lanzada si no encuentra un navegador
     except webbrowser.Error:
-        print("No se ha encontrado navegador.")
+        messagebox.showwarning("Error", "No se ha encontrado navegador.")
 
-    while(opcion!='1' and opcion!='2'):
-        # Muestra la url usando el navegador por defecto, si "new" es 0 la url se abre en la misma
-        # ventana del navegador si es posible. Si es 1 abre un nuevo navegador, si es 2 abre un nuevo
-        # "tab". Si "autorise" es True te trae al frente la ventana si es posible.
-        ## webbrowser.open(url, new=0, autoraise=True)
-        print("Abrir en una nueva pestaña o nueva ventana? 1-Ventana || 2-Pestaña")
-        opcion = input()
-        if(opcion == '1'):
-            # Comando para abrir la URL en una nueva ventana
-            navegador.open_new(url)
-        elif(opcion == '2'):
-            # Comando para abrir la URL en una nueva tab
-            navegador.open_new_tab(url)
-        else:
-            print("Opcion incorrecta.")
+    if(opcion == 1):
+        navegador.open_new(url)
+    elif(opcion == 2):
+        navegador.open_new_tab(url)
+    else:
+        messagebox.showwarning("Error", "Selecciona una opcion")
 
-    print("Quiere salir del programa o abrir otra web? 0-Salir || 1-Seguir")
-    salida=input()
-    if(salida == '0'):
-        print("Salida del programa.")
-    
+titulo="Navegador"
+icono = os.path.join(os.path.dirname(__file__), 'icon.ico')
+
+root = Tk()
+root.title(titulo)
+root.iconbitmap(icono)
+root.resizable(False, False)
+
+l = Label(root, text="URL")
+l.pack()
+
+e = Entry(root, width=50, justify=CENTER)
+e.pack()
+
+v = IntVar()
+Radiobutton(root, text="Nueva Ventana", variable=v, value=1).pack()
+Radiobutton(root, text="Nueva Pestaña", variable=v, value=2).pack()
+
+b = Button(root, text="Ir", command=navegar)
+b.config(bd=3,relief=GROOVE)
+b.pack(fill=BOTH, expand=1)
+
+root.mainloop()
