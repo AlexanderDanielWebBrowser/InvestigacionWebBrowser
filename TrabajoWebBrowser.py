41f3ff9eb56da40fb1a8a52edbb6fe598881a398
Alex <mitrio64@gmail.com>
2017-11-08 09:25:00 +0100

    WebBrowser comentarios

--- TrabajoWebBrowser.py
+++ TrabajoWebBrowser.py
@@ -1,3 +1,5 @@
+# Importamos webbrowser para poder usar los comandos de webbrowser
+# Importamos os para poder usar los comandos del sistema
 import webbrowser, os
 from tkinter import *
 from tkinter import messagebox
@@ -11,13 +13,17 @@ def navegar():
     url = e.get()
 
     try:
+        # webbrowser.get() permite seleccionar el navegador que usaremos, en este caso usamos el navegador por defecto
         navegador = webbrowser.get('windows-default')
+    # Si el webbrowser.get() nos da error enseñamos un mensaje que nos avisa
     except webbrowser.Error:
         messagebox.showwarning("Error", "No se ha encontrado navegador.")
 
     if(opcion == 1):
+        # navegador.open_new(url) abre una nueva ventana con la url que le pasamos
         navegador.open_new(url)
     elif(opcion == 2):
+        # navegador.open_new_tab(url) abre una nueva pestaña en el navegador con la url que le pasamos
         navegador.open_new_tab(url)
     else:
         messagebox.showwarning("Error", "Selecciona una opcion")
